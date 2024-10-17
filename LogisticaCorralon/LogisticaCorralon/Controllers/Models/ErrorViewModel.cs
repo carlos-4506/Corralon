372b0c6d2426551469a73f2de99b6c6f4773ca2b
carlos-4506 <rodriguezcarlos98@hotmail.com>
2024-10-17 18:11:55 -0300

    Delete LogisticaCorralon/LogisticaCorralon/Controllers directory

--- LogisticaCorralon/LogisticaCorralon/Controllers/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace LogisticaCorralon.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
